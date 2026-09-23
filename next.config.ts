6988b0ccd4a0404182fea42780c511e46fa29136
gurprit <gurprit.sahota@gmail.com>
2026-09-23 04:43:18 +0100

    Remove conflicting Next env override for vinext

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? '',
-  },
-};
-
-export default nextConfig;
